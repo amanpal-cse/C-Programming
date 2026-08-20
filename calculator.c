71dc553584efe785ab2adc30cf9a341e3400b049
Aman Pal <aman957551@gmail.com>
2026-08-20 19:54:53 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%f", &a);   
+    printf("Enter second number: ");
+    scanf("%f", &b);
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    switch(op)
+    {
+        case '+':
+            printf("Result = %.2f", a + b);
+            break;
+
+        case '-':
+            printf("Result = %.2f", a - b);
+            break;
+
+        case '*':
+            printf("Result = %.2f", a * b);
+            break;
+
+        case '/':
+            if(b != 0)
+                printf("Result = %.2f", a / b);
+            else
+                printf("Cannot divide by zero");
+            break;
+
+        default:
+            printf("Invalid operator");
+    }
+    return 0;
+}
