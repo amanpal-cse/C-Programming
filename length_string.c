9a01f01bd3db38038b972208d7208f23f4770370
Aman Pal <aman957551@gmail.com>
2026-09-15 19:04:29 +0530

    Add files via upload

---
+++ length_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int length;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    length = strlen(str);
+
+    printf("Length of string = %d", length);
+
+    return 0;
+}
